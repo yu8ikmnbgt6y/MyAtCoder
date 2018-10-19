d07e2b91907d082e03a91f87b3f4eceaa6f37ca3
yu8ikmnbgt6y <Undefined>
2018-10-19 18:39:58 +0900

    Add comments, remove unnecessary check

--- python/ABC112/D_Partition/D_Partition.py
+++ python/ABC112/D_Partition/D_Partition.py
@@ -2,19 +2,24 @@ import math
 N, M = map(int, input().split())
 
 
+# Exapmle:
+# enum_divisors(10) returns [1,2,5,10]
 def enum_divisors(n, reverse=False):
     divisors = []
+    # Each divisor has its own pair.
+    # So, it is not necessary to calculate up to n.
+    # Calculation up to sqrt(n) is enough.
     for i in range(1, int(math.sqrt(n)) + 1):
         if n % i == 0:
             divisors.append(i)
             divisors.append(n//i)
     return sorted(divisors, reverse=reverse)
- 
- 
+
+
+# The Solution is the maximum among the integers meeting following conditions.
+# (1) One of the divisors of M
+# (2) <= int(M/N)
 divisors = enum_divisors(M, reverse=True)
-divisors = filter(lambda x: x <= M//N, divisors)
- 
-for cand in divisors:
-    if M % cand == 0:
-        print(cand)
-        exit()
+divisors = list(filter(lambda x: x <= M//N, divisors))
+
+print(divisors[0])
