a2cbc996cfca7daf796feb86c63c099bc0f1ccd5
yu8ikmnbgt6y <Undefined>
2018-10-19 16:47:39 +0900

    format correction

--- python/ABC111/AtCoderBeginnerContest111.py
+++ python/ABC111/AtCoderBeginnerContest111.py
@@ -1,5 +1,5 @@
 num = int(input())
-for item in [i*111 for i in range(1,10)]:
-  if num <= item:
-    print(item)
-    break
+for item in [i*111 for i in range(1, 10)]:
+    if num <= item:
+        print(item)
+        break
